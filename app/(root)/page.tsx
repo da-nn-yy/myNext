59c3064f270bc185aa47ea2840966dca01d5698b
da-nn-yy <db7394928@gmail.com>
2024-12-26 08:26:19 +0300

    aoading and api routing done

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,18 @@
-
-
-export default function Home() {
-  console.log('Home Page');
+async function Home() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/albums");
+  if (!response.ok) throw new Error("Failed to fetch albums");
+  const albums = await response.json();
   return (
-   <h1 className="text-4xl font-bold">Welcome to Next</h1>
-  );
-}
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols">
+      {albums.map((album:{id:number,title:string}) => (
+        <div
+            key = {album.id}
+            className="bg-white shadow-md rounded-lg p-4 m-5 transtion t...">
+            <h2 className="text-blue-600 text-lg font-bold mb-2">{album.title}</h2>
+            <p className="text-gray-600">Album ID: {album.id}</p>
+        </div>
+        ))}
+        </div>
+      );
+};
+export default Home;
